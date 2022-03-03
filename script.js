e3f7f10f3c25a575bebed53a66d86b47f15c8df7
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2022-03-03 15:56:26 +0100

    fixed a bug

--- script.js
+++ script.js
@@ -331,14 +331,14 @@ async function forcast(city = "algiers", timeOfDay = "morning") {
 
 // Get and display the user's location weather information on the webpage when it loads
 window.addEventListener("load", async function () {
-  // navigator.geolocation.getCurrentPosition(async function (position) {
-  //   let lat = position.coords.latitude;
-  //   let lon = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(async function (position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-  //   const [{ name }] = await reverseGeoCoding(lat, lon);
-  //   forcast(name);
-  // });
-  forcast("algiers");
+    const [{ name }] = await reverseGeoCoding(lat, lon);
+    forcast(name);
+  });
+  // forcast("algiers");
 });
 
 // When a new location is chosen update the UI with that location
@@ -350,12 +350,12 @@ submitBtn.addEventListener("click", async function (e) {
 // Update the UI when a new time of day is chosen, otherwise always display morning data
 select.addEventListener("change", async function (e) {
   selectValue = e.target.value === "select" ? "morning" : e.target.value;
-  // navigator.geolocation.getCurrentPosition(async function (position) {
-  //   let lat = position.coords.latitude;
-  //   let lon = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(async function (position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-  //   const [{ name }] = await reverseGeoCoding(lat, lon);
-  //   forcast(inputValue.value || name, selectValue);
-  // });
-  forcast(inputValue.value || "algiers", selectValue);
+    const [{ name }] = await reverseGeoCoding(lat, lon);
+    forcast(inputValue.value || name, selectValue);
+  });
+  // forcast(inputValue.value || "algiers", selectValue);
 });
